7d5cd5ba486bee99ac06c476e2fb897449330ad2
colnat412 <nguyentanloc1108@gmail.com>
2024-09-04 09:10:11 +0700

    filter

---
+++ Lab02/filter.js
@@ -0,0 +1,18 @@
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+Array.prototype.myFilter = function (callback) {
+  var newArray = [];
+
+  for (let i = 0; i < this.length; i++) {
+    if (callback(this[i]) == true) {
+      newArray.push(this[i]);
+    }
+  }
+
+  return newArray;
+};
+
+var evens = numbers.myFilter((item) => {
+  return item % 2 === 0;
+});
+
+console.log(evens);
