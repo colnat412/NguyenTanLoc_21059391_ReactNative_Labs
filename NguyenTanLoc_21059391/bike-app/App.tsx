d38cbf1e18684013cedd376f91d33037fef64a84
colnat412 <nguyentanloc1108@gmail.com>
2024-11-11 16:14:00 +0700

    Add Home Page + Minh Chung

--- NguyenTanLoc_21059391/bike-app/App.tsx
+++ NguyenTanLoc_21059391/bike-app/App.tsx
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StyleSheet, Text, View } from "react-native";
+import HomePage from "./app/HomePage";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <NavigationContainer>
+    //   <Stack.Navigator initialRouteName="HomePage">
+    //     <Stack.Screen name="HomePage" component={HomePage}></Stack.Screen>
+    //   </Stack.Navigator>
+    // </NavigationContainer>
+    <HomePage />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
